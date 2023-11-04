872468bcea26e53f7da5a354e2c0e18865923441
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2023-11-04 13:22:59 -0700

    First Commit

---
+++ cbx_frontend/src/constants/index.js
@@ -0,0 +1,55 @@
+import { facebook, instagram, linkedin, twitter } from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "investment-team",
+    title: "Investment Team",
+  },
+  {
+    id: "portfolio",
+    title: "Portfolio",
+  },
+  {
+    id: "partner-firms",
+    title: "Partner Firms",
+  },
+  {
+    id: "alumni",
+    title: "Alumni",
+  },
+  {
+    id: "recruiting",
+    title: "Recruiting",
+  },
+  {
+    id: "investor-portal",
+    title: "Investor-Portal",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
