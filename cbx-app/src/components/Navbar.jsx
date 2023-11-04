7d80ff4c56deb82d1b98ac365a6e5a675cbab1f4
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2023-11-04 16:34:24 -0700

    fade-in navbar animation on created

--- cbx-app/src/components/Navbar.jsx
+++ cbx-app/src/components/Navbar.jsx
@@ -4,11 +4,12 @@ import { navLinks } from '../constants'
 import "../index.css"
 
 const Navbar = () => {
+  const [FUCK, setFUCK] = useState(false)
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
         <ul className="list-none sm:flex hidden justify-end items-center flex-1">
             {navLinks.map((nav, index) =>
-            (<li key={nav.id} classname={`font-poppins font-bold text-6xl`}>
+            (<li key={nav.id} className={`font-poppins px-4 ${FUCK ? "opacity-100 translate-y-0	" : "opacity-10 translate-y-5"} transition duration-500 ease-in-out`} onClick={()=>setFUCK(true)}>
                 <a href={`#${nav.id}`}>
                     {nav.title}
                 </a>
