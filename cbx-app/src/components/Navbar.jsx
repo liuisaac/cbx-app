7e375d2a27a70505c90786075a2bef6846f91253
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2023-11-04 19:26:36 -0700

    Render transition with useEffect hook

--- cbx-app/src/components/Navbar.jsx
+++ cbx-app/src/components/Navbar.jsx
@@ -1,22 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { navLinks } from '../constants'
 import "../index.css"
 
 const Navbar = () => {
-  const [FUCK, setFUCK] = useState(false)
+  const [transition, setTransition] = useState(false)
+  useEffect(() => { 
+    const timeout = setTimeout(() => { 
+      setTransition(true); 
+    }, 10);
+    return () => clearTimeout(timeout); 
+  }, []);  
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
         <ul className="list-none sm:flex hidden justify-end items-center flex-1">
             {navLinks.map((nav, index) =>
-            (<li key={nav.id} className={`font-poppins px-4 ${FUCK ? "opacity-100 translate-y-0	" : "opacity-10 translate-y-5"} transition duration-500 ease-in-out`} onClick={()=>setFUCK(true)}>
+            (<li key={nav.id} className={`font-poppins px-4 transition ${transition ? `opacity-100 translate-y-0` : `opacity-0 translate-y-7`} transition duration-1000 ease-in-out`} style={{transitionDelay : `${String(300*index)}ms`}}>
                 <a href={`#${nav.id}`}>
-                    {nav.title}
+                    {nav.title} 
                 </a>
             </li>))}
         </ul>
     </nav>
   )
 }
+
 
 export default Navbar
--- cbx-app/tailwind.config.js
+++ cbx-app/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionDelay: {
+        1000: "1000ms",
+        1500: "1500ms",
+        2000: "2000ms",
+        2500: "2500ms",
+        3000: "3000ms",
+      },
+    },
   },
   plugins: [],
 };
