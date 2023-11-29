bea1bd7fbf307b3a1c9580875edb49a024a424c1
htreem <hoganj.ma@gmail.com>
2023-11-29 17:52:01 +0000

    completed transition F

--- cbx/src/App.js
+++ cbx/src/App.js
@@ -5,10 +5,15 @@ import TransitionF from'./Transition-F/Transition-F.jsx';
 import TransitionG from'./Transition-G/Transition-G.jsx';
 
 function App() {
+  const transitionFText = [
+    "We are partnered with XX firms with XX millions in AUM filler text. ",
+    "Second line of the paragraph asdfa sdfadsfad sfad sfasdfa dsfadsfadsfa daf a. ",
+    "Third line of the paragraphasdfasfa dsfasfaafsdf",
+  ];
   return (
     <div>
       <TransitionC/>
-      <TransitionF/>
+      <TransitionF text={transitionFText}/>
       <TransitionG/>
       <div className="App">
         <header className="App-header">
