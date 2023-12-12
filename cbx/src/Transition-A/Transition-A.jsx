414d8ed6d214f0303adee86e15732bb1bf666a81
Terence <zunmun@gmail.com>
2023-12-12 11:52:22 +0800

    feat: rough fade animation

--- cbx/src/Transition-A/Transition-A.jsx
+++ cbx/src/Transition-A/Transition-A.jsx
@@ -1,5 +1,7 @@
 import "./Transition-A.css";
 import logo from './CBX_white.png';
+import logoCbx from './CBX_white_cbx.png';
+import logoPartners from './CBX_white_partners.png';
 import background from "./Home_Page_Background.jpg";
 
 function TransitionA() {
@@ -9,10 +11,17 @@ function TransitionA() {
       backgroundRepeat: 'no-repeat', 
       backgroundPosition: 'center',
       backgroundSize: 'cover',
+      
     }} class="bg-black">
-      <div class="md:w-5/6 lg:w-4/6 px-5 py-24 mx-auto h-screen flex">
-        <div class="m-auto transitionA flex justify-center w-3/4"  alt="logo">
-            <img src={logo} class="inline p-0" alt="logo" />
+      <div class="md:w-5/6 lg:w-4/6 px-5 py-24 m-auto h-screen flex">
+        <div class="m-auto transitionACbx flex justify-center"  alt="logo">
+            <img src={logoCbx} class="inline p-0" alt="logo" />
+        </div>
+        <div class="m-auto p-5 flex justify-center display-inline"  alt="logo">
+          <div class="slash"></div>
+        </div>
+        <div class="m-auto transitionAPartners flex justify-center w-3/4"  alt="logo">
+            <img src={logoPartners} class="inline p-0" alt="logo" />
         </div>
       </div>
     </div>
