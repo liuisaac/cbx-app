aa0247665e395bbe87b609d934fca2afb48d20ee
Terence <zunmun@gmail.com>
2023-12-13 06:52:15 +0800

    feat: line animation

--- cbx/src/Transition-A/Transition-A.jsx
+++ cbx/src/Transition-A/Transition-A.jsx
@@ -15,13 +15,13 @@ function TransitionA() {
     }} class="bg-black">
       <div class="md:w-5/6 lg:w-4/6 px-5 py-24 m-auto h-screen flex">
         <div class="m-auto transitionACbx flex justify-center"  alt="logo">
-            <img src={logoCbx} class="inline p-0" alt="logo" />
+            <img src={logoCbx} class="inline p-0 h-24  object-scale-down" alt="logo" />
         </div>
         <div class="m-auto p-5 flex justify-center display-inline"  alt="logo">
-          <div class="slash"></div>
+          <div class="transitionASlash"></div>
         </div>
         <div class="m-auto transitionAPartners flex justify-center w-3/4"  alt="logo">
-            <img src={logoPartners} class="inline p-0" alt="logo" />
+            <img src={logoPartners} class="inline p-0 h-24 object-scale-down" alt="logo" />
         </div>
       </div>
     </div>
