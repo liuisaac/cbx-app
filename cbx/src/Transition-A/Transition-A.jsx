25d9c579ef62fa0d1393fbd2771129ee4b9b4766
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2023-12-21 16:01:15 -0700

    Added alt text to logo

--- cbx/src/Transition-A/Transition-A.jsx
+++ cbx/src/Transition-A/Transition-A.jsx
@@ -13,7 +13,7 @@ const TransitionA = () => {
   return (
     <div>
         <div className={`mt-8 flex justify-center font-poppins px-4 transition duration-[2000ms] ${transition ? `opacity-100 translate-y-0` : `opacity-0 translate-y-10`} transition ease-in-out w-full h-full`}>
-            <img src={ logo } />
+            <img src={ logo } alt='CBX Partners Logo'/>
         </div>
     </div>
   )
